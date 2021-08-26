ed13c7b61c2bc95fc931f583ff52e1519486bd76
shockliang <shockwater@gmail.com>
2021-08-26 20:41:24 +0800

    Fetching activities.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [activites, setActivities] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/activities')
+            .then(response => {
+                console.log(response);
+                setActivities(response.data);
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <ul>
+                    {activites.map((activity: any) => (
+                        <li key={activity.id}>
+                            {activity.title}
+                        </li>
+                    ))}
+                </ul>
+            </header>
+        </div>
+    );
 }
 
 export default App;
