b1f11b39913aed54090e75f7ef0314367ef73c8f
shockliang <shockwater@gmail.com>
2021-08-31 01:12:06 +0800

    Adding an edit handler.

---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,37 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+        
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _dataContext;
+
+            public Handler(DataContext dataContext)
+            {
+                _dataContext = dataContext;
+            }
+            
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _dataContext.Activities.FindAsync(request.Activity.Id);
+                
+                activity.Title = request.Activity.Title ?? activity.Title;
+
+                await _dataContext.SaveChangesAsync();
+                
+                return Unit.Value;
+            }
+        }
+    }
+}
