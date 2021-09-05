249ed5b12efe253d9fd2dbd0e7edc944d30d8e5b
shockliang <shockwater@gmail.com>
2021-09-06 01:17:50 +0800

    Sytling the nav bar.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import {Header, List} from "semantic-ui-react";
+import {Header, List, Container} from "semantic-ui-react";
 import {Activity} from "../models/activity";
 import NavBar from "./NavBar";
 
@@ -18,16 +18,18 @@ function App() {
     }, []);
 
     return (
-        <div>
+        <>
             <NavBar />
-            <List>
-                {activities.map((activity) => (
-                    <List.Item key={activity.id}>
-                        {activity.title}
-                    </List.Item>
-                ))}
-            </List>
-        </div>
+            <Container style={{marginTop: '7em'}}>
+                <List>
+                    {activities.map((activity) => (
+                        <List.Item key={activity.id}>
+                            {activity.title}
+                        </List.Item>
+                    ))}
+                </List>
+            </Container>
+        </>
     );
 }
 
