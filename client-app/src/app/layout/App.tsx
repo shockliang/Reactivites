2e422544d03f4b007ee7f086b4b8266831f61802
shockliang <shockwater@gmail.com>
2021-09-11 00:49:22 +0800

    Displaying the create/edit form.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import {Header, List, Container} from "semantic-ui-react";
+import {Container} from "semantic-ui-react";
 import {Activity} from "../models/activity";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
@@ -8,6 +8,7 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 function App() {
     const [activities, setActivities] = useState<Activity[]>([]);
     const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+    const [editMode, setEditMode] = useState(false);
 
     useEffect(() => {
         axios.get<Activity[]>('http://localhost:5000/api/activities')
@@ -27,15 +28,27 @@ function App() {
         setSelectedActivity(undefined);
     }
 
+    function handleFormOpen(id?: string) {
+        id ? handleSelectActivity(id) : handleCancelSelectActivity();
+        setEditMode(true);
+    }
+
+    function handleFormClose() {
+        setEditMode(false);
+    }
+
     return (
         <>
-            <NavBar/>
+            <NavBar openForm={handleFormOpen}/>
             <Container style={{marginTop: '7em'}}>
                 <ActivityDashboard
                     activities={activities}
                     selectedActivity={selectedActivity}
                     selectActivity={handleSelectActivity}
                     cancelSelectActivity={handleCancelSelectActivity}
+                    editMode={editMode}
+                    openForm={handleFormOpen}
+                    closeForm={handleFormClose}
                 />
             </Container>
         </>
