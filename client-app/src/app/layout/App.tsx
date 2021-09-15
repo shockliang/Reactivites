aa02793c970c72f2a27460ea8521d2358f5f4aa8
shockliang <shockwater@gmail.com>
2021-09-15 12:22:56 +0800

    Deleting an activity.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -46,6 +46,10 @@ function App() {
         setSelectedActivity(activity);
     }
 
+    function handleDeleteActivity(id: string) {
+        setActivities([...activities.filter(x => x.id !== id)]);
+    }
+
     return (
         <>
             <NavBar openForm={handleFormOpen}/>
@@ -59,6 +63,7 @@ function App() {
                     openForm={handleFormOpen}
                     closeForm={handleFormClose}
                     createOrEdit={handleCreateOrEditActivity}
+                    deleteActivity={handleDeleteActivity}
                 />
             </Container>
         </>
