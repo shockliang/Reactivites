540fa2811b7bf15a36912fe7d67deea3f1887387
shockliang <shockwater@gmail.com>
2021-09-18 00:00:30 +0800

    Adding loading component.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,16 +1,17 @@
 import React, {useEffect, useState} from 'react';
-import axios from "axios";
 import {Container} from "semantic-ui-react";
 import {Activity} from "../models/activity";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import {v4 as uuid} from 'uuid';
 import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
     const [activities, setActivities] = useState<Activity[]>([]);
     const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
     const [editMode, setEditMode] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         agent.Activities.list()
@@ -21,6 +22,7 @@ function App() {
                     activities.push(activity);
                 });
                 setActivities(activities);
+                setLoading(false);
             })
             .catch(error => {
                 console.log(error)
@@ -55,6 +57,8 @@ function App() {
     function handleDeleteActivity(id: string) {
         setActivities([...activities.filter(x => x.id !== id)]);
     }
+
+    if (loading) return <LoadingComponent content={'Loading app...'}/>
 
     return (
         <>
