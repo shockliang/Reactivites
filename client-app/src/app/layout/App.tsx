f03c51b0525ead1e963c4d7e92c307718fe10c32
shockliang <shockwater@gmail.com>
2021-09-22 00:35:47 +0800

    Refactoring the app to use MobX.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,6 +7,7 @@ import {v4 as uuid} from 'uuid';
 import agent from "../api/agent";
 import LoadingComponent from "./LoadingComponent";
 import {useStore} from "../stores/store";
+import {observer} from "mobx-react-lite";
 
 function App() {
     const {activityStore} = useStore();
@@ -14,24 +15,11 @@ function App() {
     const [activities, setActivities] = useState<Activity[]>([]);
     const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
     const [editMode, setEditMode] = useState(false);
-    const [loading, setLoading] = useState(true);
     const [submitting, setSubmitting] = useState(false);
 
     useEffect(() => {
-        agent.Activities.list()
-            .then(response => {
-                let activities: Activity[] = [];
-                response.forEach(activity => {
-                    activity.date = activity.date.split('T')[0];
-                    activities.push(activity);
-                });
-                setActivities(activities);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.log(error)
-            });
-    }, []);
+        activityStore.loadActivities();
+    }, [activityStore]);
 
     function handleSelectActivity(id: string) {
         setSelectedActivity(activities.find(x => x.id === id));
@@ -52,7 +40,7 @@ function App() {
 
     function handleCreateOrEditActivity(activity: Activity) {
         setSubmitting(true);
-        if(activity.id) {
+        if (activity.id) {
             agent.Activities.update(activity).then(() => {
                 setActivities([...activities.filter(x => x.id !== activity.id), activity]);
                 setSelectedActivity(activity);
@@ -78,14 +66,14 @@ function App() {
         });
     }
 
-    if (loading) return <LoadingComponent content={'Loading app...'}/>
+    if (activityStore.loadingInitial) return <LoadingComponent content={'Loading app...'}/>
 
     return (
         <>
             <NavBar openForm={handleFormOpen}/>
             <Container style={{marginTop: '7em'}}>
                 <ActivityDashboard
-                    activities={activities}
+                    activities={activityStore.activities}
                     selectedActivity={selectedActivity}
                     selectActivity={handleSelectActivity}
                     cancelSelectActivity={handleCancelSelectActivity}
@@ -101,4 +89,4 @@ function App() {
     );
 }
 
-export default App;
+export default observer(App);
