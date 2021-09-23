66ff04ee57e83f54b14d50406f757ef86643a6ce
shockliang <shockwater@gmail.com>
2021-09-23 14:18:21 +0800

    Selecting an activity using MobX.

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import {Button, Card, Image} from "semantic-ui-react";
-import {Activity} from "../../../app/models/activity";
+import {useStore} from "../../../app/stores/store";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
-interface Props {
-    activity: Activity;
-    cancelSelectActivity: () => void;
-    openForm: (id: string) => void;
+export default function ActivityDetails() {
+    const {activityStore} = useStore();
+    const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
 
-}
+    if (!activity) return <LoadingComponent content={''}/>;
 
-export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props) {
     return (
         <Card fluid>
-            <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+            <Image src={`/assets/categoryImages/${activity.category}.jpg`}/>
             <Card.Content>
                 <Card.Header>{activity.title}</Card.Header>
                 <Card.Meta>
@@ -23,10 +22,10 @@ export default function ActivityDetails({activity, cancelSelectActivity, openFor
                 </Card.Description>
             </Card.Content>
             <Card.Content extra>
-               <Button.Group widths={'2'}>
-                   <Button onClick={() => openForm(activity.id)} basic color={'blue'} content={'Edit'} />
-                   <Button onClick={cancelSelectActivity} basic color={'grey'} content={'Cancel'} />
-               </Button.Group>
+                <Button.Group widths={'2'}>
+                    <Button onClick={() => openForm(activity.id)} basic color={'blue'} content={'Edit'}/>
+                    <Button onClick={cancelSelectedActivity} basic color={'grey'} content={'Cancel'}/>
+                </Button.Group>
             </Card.Content>
         </Card>
     )
