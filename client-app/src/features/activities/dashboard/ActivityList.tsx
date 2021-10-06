48ae78a70aeda914ec2ca7e93ab153825f14e7e6
shockliang <shockwater@gmail.com>
2021-10-07 00:26:46 +0800

    Grouping the activities by date.

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,20 +1,30 @@
-import React from "react";
-import {Segment, Item} from "semantic-ui-react";
+import React, {Fragment} from "react";
+import {Segment, Item, Header} from "semantic-ui-react";
 import {useStore} from "../../../app/stores/store";
 import {observer} from "mobx-react-lite";
 import ActivityListItem from "./ActivityListItem";
 
 export default observer(function ActivityList() {
     const {activityStore} = useStore();
-    const {activitiesByDate} = activityStore;
+    const {groupedActivities} = activityStore;
 
     return (
-        <Segment>
-            <Item.Group divided>
-                {activitiesByDate.map(activity => (
-                    <ActivityListItem key={activity.id} activity={activity} />
-                ))}
-            </Item.Group>
-        </Segment>
+        <>
+            {groupedActivities.map(([group, activities]) => (
+                <Fragment key={group}>
+                    <Header sub color={'teal'}>
+                        {group}
+                    </Header>
+                    <Segment>
+                        <Item.Group divided>
+                            {activities.map(activity => (
+                                <ActivityListItem key={activity.id} activity={activity}/>
+                            ))}
+                        </Item.Group>
+                    </Segment>
+                </Fragment>
+            ))}
+        </>
+
     )
 })
