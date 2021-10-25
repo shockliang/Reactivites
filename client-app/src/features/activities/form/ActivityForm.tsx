46ccac95109381e8b90d89273a1fac0aa5ae2f0b
shockliang <shockwater@gmail.com>
2021-10-26 00:06:13 +0800

    Creating a reusable text input.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -5,7 +5,9 @@ import {observer} from "mobx-react-lite";
 import {Link, useHistory, useParams} from "react-router-dom";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import {v4 as uuid} from 'uuid';
-import {Formik, Form, Field} from "formik";
+import {Formik, Form} from "formik";
+import * as Yup from 'yup';
+import MyTextInput from "../../../app/common/form/MyTextInput";
 
 export default observer(function ActivityForm() {
     const history = useHistory();
@@ -21,6 +23,15 @@ export default observer(function ActivityForm() {
         city: '',
         venue: ''
     });
+
+    const validationSchema = Yup.object({
+        title: Yup.string().required('The activity title is required'),
+        description: Yup.string().required('The activity description is required'),
+        category: Yup.string().required(),
+        date: Yup.string().required(),
+        venue: Yup.string().required(),
+        city: Yup.string().required(),
+    })
 
     useEffect(() => {
         if (id) loadActivity(id).then(activity => setActivity(activity!));
@@ -47,15 +58,19 @@ export default observer(function ActivityForm() {
 
     return (
         <Segment clearing>
-            <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
+            <Formik
+                validationSchema={validationSchema}
+                enableReinitialize
+                initialValues={activity}
+                onSubmit={values => console.log(values)}>
                 {({handleSubmit}) => (
                     <Form className={'ui form'} onSubmit={handleSubmit} autoComplete={'off'}>
-                        <Field placeholder={'Title'} name={'title'}/>
-                        <Field placeholder={'Description'} name={'description'}/>
-                        <Field placeholder={'Category'} name={'category'}/>
-                        <Field type={'date'} placeholder={'Date'} name={'date'}/>
-                        <Field placeholder={'City'} name={'city'}/>
-                        <Field placeholder={'Venue'} name={'venue'}/>
+                        <MyTextInput placeholder={'title'} name={'title'} />
+                        <MyTextInput placeholder={'Description'} name={'description'}/>
+                        <MyTextInput placeholder={'Category'} name={'category'}/>
+                        <MyTextInput placeholder={'Date'} name={'date'}/>
+                        <MyTextInput placeholder={'City'} name={'city'}/>
+                        <MyTextInput placeholder={'Venue'} name={'venue'}/>
                         <Button loading={loading} floated={'right'} positive type={'submit'} content={'Submit'}/>
                         <Button as={Link} to={'/activities'} floated={'right'} type={'button'} content={'Cancel'}/>
                     </Form>
