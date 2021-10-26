c074e045a29783c7130beb034124a282d13cd15c
shockliang <shockwater@gmail.com>
2021-10-27 00:11:49 +0800

    Creating a reusable text area.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,6 +8,7 @@ import {v4 as uuid} from 'uuid';
 import {Formik, Form} from "formik";
 import * as Yup from 'yup';
 import MyTextInput from "../../../app/common/form/MyTextInput";
+import MyTextArea from "../../../app/common/form/MyTextArea";
 
 export default observer(function ActivityForm() {
     const history = useHistory();
@@ -66,7 +67,7 @@ export default observer(function ActivityForm() {
                 {({handleSubmit}) => (
                     <Form className={'ui form'} onSubmit={handleSubmit} autoComplete={'off'}>
                         <MyTextInput placeholder={'title'} name={'title'} />
-                        <MyTextInput placeholder={'Description'} name={'description'}/>
+                        <MyTextArea rows={3} placeholder={'Description'} name={'description'}/>
                         <MyTextInput placeholder={'Category'} name={'category'}/>
                         <MyTextInput placeholder={'Date'} name={'date'}/>
                         <MyTextInput placeholder={'City'} name={'city'}/>
