7229ae326ce2421fe4f8a1fc94b2c4ccaae87e4f
shockliang <shockwater@gmail.com>
2021-11-26 00:05:02 +0800

    Shaping the related data.

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -11,23 +12,31 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>>
+        public class Query : IRequest<Result<List<ActivityDto>>>
         {
             
         }
         
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
             private readonly DataContext _dataContext;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext dataContext)
+            public Handler(DataContext dataContext, IMapper mapper)
             {
                 _dataContext = dataContext;
+                _mapper = mapper;
             }
             
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return Result<List<Activity>>.Success(await _dataContext.Activities.ToListAsync(cancellationToken));
+                var activities = await _dataContext.Activities
+                    .Include(a => a.Attendees)
+                    .ThenInclude(u => u.AppUser)
+                    .ToListAsync(cancellationToken);
+                var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
+                
+                return Result<List<ActivityDto>>.Success(activitiesToReturn);
             }
         }
     }
