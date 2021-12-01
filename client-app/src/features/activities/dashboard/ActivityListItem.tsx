5f0e871d338a14dba662f71e328ee727de915f04
shockliang <shockwater@gmail.com>
2021-12-02 00:00:30 +0800

    Adding the attendees component.

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -3,6 +3,7 @@ import {Button, Icon, Item, Segment} from "semantic-ui-react";
 import {Link} from "react-router-dom";
 import {Activity} from "../../../app/models/activity";
 import {format} from "date-fns";
+import ActivityListItemAttendee from "./ActivityListItemAttendee";
 
 interface Props {
     activity: Activity
@@ -32,7 +33,7 @@ export default function ActivityListItem({activity}: Props) {
                 </span>
             </Segment>
             <Segment secondary>
-                Attendees go here
+                <ActivityListItemAttendee attendees={activity.attendees!}/>
             </Segment>
             <Segment clearing>
                 <span>{activity.description}</span>
