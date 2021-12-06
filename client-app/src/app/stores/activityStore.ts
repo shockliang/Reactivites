3f26d2afb83856ebb7462d676b976708882a736f
shockliang <shockwater@gmail.com>
2021-12-07 00:27:19 +0800

    Adding a cancel activity method.

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -159,4 +159,20 @@ export default class ActivityStore {
             runInAction(() => this.loading = false);
         }
     }
+
+    cancelActivityToggle = async () => {
+        this.loading = true;
+        try {
+            await agent.Activities.attend(this.selectedActivity!.id);
+            runInAction(() => {
+                this.selectedActivity!.isCancelled = !this.selectedActivity?.isCancelled;
+                this.activityRegistry.set(this.selectedActivity!.id, this.selectedActivity!);
+            });
+
+        } catch (e) {
+            console.log(e);
+        } finally {
+            runInAction(() => this.loading = false);
+        }
+    }
 }
