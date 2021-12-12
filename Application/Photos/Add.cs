3677bec09e63c89b811cc9452f78ee3c4aaba851
shockliang <shockwater@gmail.com>
2021-12-13 00:49:53 +0800

    Adding the add photot handler.

---
+++ Application/Photos/Add.cs
@@ -0,0 +1,63 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Photos
+{
+    public class Add
+    {
+        public class Command : IRequest<Result<Photo>>
+        {
+            public IFormFile File { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Photo>>
+        {
+            private readonly DataContext _dataContext;
+            private readonly IPhotoAccessor _photoAccessor;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext dataContext, IPhotoAccessor photoAccessor, IUserAccessor userAccessor)
+            {
+                _dataContext = dataContext;
+                _photoAccessor = photoAccessor;
+                _userAccessor = userAccessor;
+            }
+            
+            public async Task<Result<Photo>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await _dataContext.Users.Include(x => x.Photos)
+                    .FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUserName());
+
+                if (user == null) return null;
+
+                var photoUploadResult = await _photoAccessor.AddPhoto(request.File);
+
+                var photo = new Photo
+                {
+                    Url = photoUploadResult.Url,
+                    Id = photoUploadResult.PublicId,
+                };
+
+                if (!user.Photos.Any(x => x.IsMain)) photo.IsMain = true;
+                
+                user.Photos.Add(photo);
+
+                var result = await _dataContext.SaveChangesAsync() > 0;
+
+                if (result) return Result<Photo>.Success();
+
+                return Result<Photo>.Failure();
+            }
+        }
+    }
+    
+    
+}
