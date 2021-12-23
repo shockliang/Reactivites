5670cb9a610383fe9dc02f1602b90395a6a8a4cb
shockliang <shockwater@gmail.com>
2021-12-24 00:21:22 +0800

    Setting up the profile data to component.

--- client-app/src/features/profiles/ProfileHeader.tsx
+++ client-app/src/features/profiles/ProfileHeader.tsx
@@ -1,29 +1,35 @@
 import React from "react";
 import {Button, Divider, Grid, Header, Item, Reveal, Segment, Statistic} from "semantic-ui-react";
+import {Profile} from "../../app/models/profile";
+import {observer} from "mobx-react-lite";
 
-export default function ProfileHeader() {
+interface Props {
+    profile: Profile
+}
+
+export default observer(function ProfileHeader({profile}: Props) {
     return (
         <Segment>
             <Grid>
                 <Grid.Column width={12}>
                     <Item.Group>
                         <Item>
-                            <Item.Image avata size={'small'} src={'/assets/user.png'} />
+                            <Item.Image avata size={'small'} src={profile.image || '/assets/user.png'}/>
                             <Item.Content verticalAlign={'middle'}>
-                                <Header as={'h1'} content={'DisplayName'} />
+                                <Header as={'h1'} content={profile.displayName}/>
                             </Item.Content>
                         </Item>
                     </Item.Group>
                 </Grid.Column>
                 <Grid.Column width={4}>
                     <Statistic.Group widths={2}>
-                        <Statistic label={'Followers'} value={5} />
-                        <Statistic label={'Following'} value={42} />
+                        <Statistic label={'Followers'} value={5}/>
+                        <Statistic label={'Following'} value={42}/>
                     </Statistic.Group>
-                    <Divider />
+                    <Divider/>
                     <Reveal animated={'move'}>
                         <Reveal.Content visible style={{width: '100%'}}>
-                            <Button fluid  color={'teal'} content={'Following'}/>
+                            <Button fluid color={'teal'} content={'Following'}/>
                         </Reveal.Content>
                         <Reveal.Content hidden style={{width: '100%'}}>
                             <Button
@@ -38,4 +44,4 @@ export default function ProfileHeader() {
             </Grid>
         </Segment>
     )
-}
+})
