691c01a92909bdf2e0816b29c270da80c56f9c05
shockliang <shockwater@gmail.com>
2022-01-11 20:55:25 +0800

    Sending comments.

--- client-app/src/features/activities/details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -1,7 +1,9 @@
 import {observer} from 'mobx-react-lite'
 import React, {useEffect} from 'react'
-import {Segment, Header, Comment, Form, Button} from 'semantic-ui-react'
+import {Segment, Header, Comment, Button} from 'semantic-ui-react'
 import {useStore} from "../../../app/stores/store";
+import {Formik, Form} from "formik";
+import MyTextArea from "../../../app/common/form/MyTextArea";
 
 interface Props {
     activityId: string;
@@ -31,7 +33,7 @@ export default observer(function ActivityDetailedChat({activityId}: Props) {
             >
                 <Header>Chat about this event</Header>
             </Segment>
-            <Segment attached>
+            <Segment attached clearing>
                 <Comment.Group>
                     {commentStore.comments.map(comment => (
                         <Comment key={comment.id}>
@@ -47,16 +49,29 @@ export default observer(function ActivityDetailedChat({activityId}: Props) {
                             </Comment.Content>
                         </Comment>
                     ))}
+                    <Formik
+                        onSubmit={(values, {resetForm}) =>
+                            commentStore.addComment(values).then(() => resetForm())}
+                        initialValues={{body: ''}}
+                    >
+                        {({isSubmitting, isValid}) => (
+                            <Form className={'ui form'}>
+                                <MyTextArea placeholder={'Add comment'} name={'body'} rows={2} />
+                                <Button
+                                    loading={isSubmitting}
+                                    disabled={isSubmitting || !isValid}
+                                    content='Add Reply'
+                                    labelPosition='left'
+                                    icon='edit'
+                                    primary
+                                    type={'submit'}
+                                    floated={'right'}
+                                />
+                            </Form>
+                        )}
+                    </Formik>
 
-                    <Form reply>
-                        <Form.TextArea/>
-                        <Button
-                            content='Add Reply'
-                            labelPosition='left'
-                            icon='edit'
-                            primary
-                        />
-                    </Form>
+
                 </Comment.Group>
             </Segment>
         </>
