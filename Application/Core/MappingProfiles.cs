a362bcd029f8fa17091468ddfaef7312756c05b1
shockliang <shockwater@gmail.com>
2022-01-17 01:02:30 +0800

    Update the profile class.

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -22,7 +22,9 @@ namespace Application.Core
                 .ForMember(d => d.Bio, options => options.MapFrom(s => s.AppUser.Bio))
                 .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
             CreateMap<AppUser, Profiles.Profile>()
-                .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count))
+                .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count));
             CreateMap<Comment, CommentDto>()
                 .ForMember(d => d.DisplayName, options => options.MapFrom(s => s.Author.DisplayName))
                 .ForMember(d => d.Username, options => options.MapFrom(s => s.Author.UserName))
