d6c18f08ebb832a40506985bb042699e33fb1393
shockliang <shockwater@gmail.com>
2022-01-23 21:15:15 +0800

    Making the follow button as component.

--- client-app/src/features/profiles/ProfileCard.tsx
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -3,6 +3,7 @@ import {Profile} from "../../app/models/profile";
 import {observer} from "mobx-react-lite";
 import {Link} from "react-router-dom";
 import { Card, Image, Icon } from "semantic-ui-react";
+import FollowButton from "./FollowButton";
 
 interface Props {
     profile: Profile;
@@ -20,6 +21,7 @@ export default observer(function ProfileCard({profile}: Props) {
                 <Icon name={'user'} />
                 {profile.followersCount} followers
             </Card.Content>
+            <FollowButton profile={profile} />
         </Card>
     )
 })
