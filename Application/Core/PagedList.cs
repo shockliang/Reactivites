4320d6588e3602b2436dd267cb8a21e62bf470ae
shockliang <shockwater@gmail.com>
2022-01-26 23:18:04 +0800

    Typo.

--- Application/Core/PagedList.cs
+++ Application/Core/PagedList.cs
@@ -6,9 +6,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Application.Core
 {
-    public class PageList<T> : List<T>
+    public class PagedList<T> : List<T>
     {
-        public PageList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
         {
             CurrentPage = pageNumber;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
@@ -22,11 +22,11 @@ namespace Application.Core
         public int PageSize { get; set; }
         public int TotalCount { get; set; }
 
-        public static async Task<PageList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
         {
             var count = await source.CountAsync();
             var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
-            return new PageList<T>(items, count, pageNumber, pageSize);
+            return new PagedList<T>(items, count, pageNumber, pageSize);
         }
     }
 }
