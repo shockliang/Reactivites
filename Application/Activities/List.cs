b0dbe9184559403a447206cd80b85ae2f770276a
shockliang <shockwater@gmail.com>
2022-01-27 00:36:53 +0800

    Adding application layer pagination logic.

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
@@ -14,12 +15,12 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<ActivityDto>>>
+        public class Query : IRequest<Result<PagedList<ActivityDto>>>
         {
-            
+            public PagingParams Params { get; set; }
         }
         
-        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
+        public class Handler : IRequestHandler<Query, Result<PagedList<ActivityDto>>>
         {
             private readonly DataContext _dataContext;
             private readonly IMapper _mapper;
@@ -32,15 +33,20 @@ namespace Application.Activities
                 _userAccessor = userAccessor;
             }
 
-            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<PagedList<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activities = await _dataContext.Activities
+                var query = _dataContext.Activities
                     .ProjectTo<ActivityDto>(
                         _mapper.ConfigurationProvider,
                         new { currentUsername = _userAccessor.GetUserName() })
-                    .ToListAsync(cancellationToken);
+                    .AsQueryable();
 
-                return Result<List<ActivityDto>>.Success(activities);
+                return Result<PagedList<ActivityDto>>.Success(
+                    await PagedList<ActivityDto>.CreateAsync(
+                        query, 
+                        request.Params.PageNumber, 
+                        request.Params.PageSize)
+                    );
             }
         }
     }
