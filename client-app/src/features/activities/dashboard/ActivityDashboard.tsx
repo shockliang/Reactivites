a42c3bf0251f1ca7ba57f51c14c6c69b098fae48
shockliang <shockwater@gmail.com>
2022-02-05 00:22:23 +0800

    Adding placeholders.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,6 +7,7 @@ import LoadingComponent from "../../../app/layout/LoadingComponent";
 import ActivityFilter from "./ActivityFilters";
 import {PagingParams} from "../../../app/models/pagination";
 import InfiniteScroll from 'react-infinite-scroller';
+import ActivityListItemPlaceholder from "./ActivityListItemPlaceholder";
 
 export default observer(function ActivityDashboard() {
     const {activityStore} = useStore();
@@ -23,19 +24,24 @@ export default observer(function ActivityDashboard() {
         if (activityRegistry.size <= 1) loadActivities();
     }, [activityRegistry.size, loadActivities]);
 
-    if (activityStore.loadingInitial && !loadingNext) return <LoadingComponent content={'Loading activities...'}/>
-
     return (
         <Grid>
             <Grid.Column width={'10'}>
-                <InfiniteScroll
-                    pageStart={0}
-                    loadMore={handleGetNext}
-                    hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
-                    initialLoad={false}
-                >
-                    <ActivityList/>
-                </InfiniteScroll>
+                {activityStore.loadingInitial && !loadingNext ? (
+                    <>
+                        <ActivityListItemPlaceholder/>
+                        <ActivityListItemPlaceholder/>
+                    </>
+                ) : (
+                    <InfiniteScroll
+                        pageStart={0}
+                        loadMore={handleGetNext}
+                        hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+                        initialLoad={false}
+                    >
+                        <ActivityList/>
+                    </InfiniteScroll>
+                )}
             </Grid.Column>
             <Grid.Column width={'6'}>
                 <ActivityFilter/>
