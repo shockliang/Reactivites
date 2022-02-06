fcf83983202308d515b100ed13386a6c9c009d12
shockliang <shockwater@gmail.com>
2022-02-07 00:04:30 +0800

    Adding a profile activities component.

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,7 +4,7 @@ import {toast} from "react-toastify";
 import {history} from "../../index";
 import {store} from "../stores/store";
 import {User, UserFormValues} from "../models/user";
-import {Photo, Profile} from "../models/profile";
+import {Photo, Profile, UserActivity} from "../models/profile";
 import {PaginatedResult} from "../models/pagination";
 
 const sleep = (delay: number) => {
@@ -106,7 +106,10 @@ const Profiles = {
     setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
     deletePhoto: (id: string) => requests.del(`/photos/${id}`),
     updateFollowing: (username: string) => requests.post(`/follow/${username}`, {}),
-    listFollowings: (username: string, predicate: string) => requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`)
+    listFollowings: (username: string, predicate: string) =>
+        requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`),
+    listActivities: (username: string, predicate: string) =>
+        requests.get<UserActivity[]>(`/profiles/${username}/activities?predicate=${predicate}`)
 }
 
 const agent = {
