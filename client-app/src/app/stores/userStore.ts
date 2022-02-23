50ac01840a74545d197c057cf94e87358cf98b94
shockliang <shockwater@gmail.com>
2022-02-24 00:29:10 +0800

    Client side configuration.

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -8,6 +8,7 @@ export default class UserStore {
     user: User | null = null;
     fbAccessToken: string | null = null;
     fbLoading = false;
+    refreshTokenTimeout: any;
 
     constructor() {
         makeAutoObservable(this)
@@ -21,6 +22,7 @@ export default class UserStore {
         try {
             const user = await agent.Account.login(creds);
             store.commonStore.setToken(user.token);
+            this.startRrefreshTokenTimer(user);
             runInAction(() => this.user = user);
             history.push('/activities');
             store.modalStore.closeModal();
@@ -41,6 +43,7 @@ export default class UserStore {
         try {
             const user = await agent.Account.current();
             runInAction(() => this.user = user);
+            this.startRrefreshTokenTimer(user);
         } catch (error) {
             console.log(error);
         }
@@ -50,6 +53,7 @@ export default class UserStore {
         try {
             const user = await agent.Account.register(creds);
             store.commonStore.setToken(user.token);
+            this.startRrefreshTokenTimer(user);
             runInAction(() => this.user = user);
             history.push('/activities');
             store.modalStore.closeModal();
@@ -76,6 +80,7 @@ export default class UserStore {
         const apiLogin = (accessToken: string) => {
             agent.Account.fbLogin(accessToken).then(user => {
                 store.commonStore.setToken(user.token);
+                this.startRrefreshTokenTimer(user);
                 runInAction(() => {
                     this.user = user;
                     this.fbLoading = false;
@@ -94,5 +99,27 @@ export default class UserStore {
                 apiLogin(response.authResponse.accessToken);
             }, {scope: 'public_profile,email'});
         }
+    }
+
+    refreshToken = async () => {
+        try {
+            const user = await agent.Account.refreshToken();
+            runInAction(() => this.user = user);
+            store.commonStore.setToken(user.token);
+            this.startRrefreshTokenTimer(user);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    private startRrefreshTokenTimer(user: User) {
+        const jwtToken = JSON.parse(atob(user.token.split('.')[1]));
+        const expires = new Date(jwtToken.exp * 1000);
+        const timeout = expires.getTime() - Date.now() - (30 * 1000);
+        this.refreshTokenTimeout = setTimeout(this.refreshToken, timeout);
+    }
+
+    private stopRereshTokenTimer() {
+        clearTimeout(this.refreshTokenTimeout);
     }
 }
